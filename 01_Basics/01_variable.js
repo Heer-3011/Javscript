f33039e486a8add92b0caceb66fa4ac15b8457d1
Heer-3011 <223sbeit54009@vsitr.ac.in>
2025-08-04 14:55:38 +0530

    new folder made

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,7 @@
+//console.log("Heer Patel ")
+const accountId=12345
+let username="Patel Heer"
+let email="patel.heer3011@gmail.com" 
+city="Surat"
+let userstate
+console.table([userstate,accountId,username,email,city])
